81b6753bc3bcfb42b5bb7cdbef7430b2b620cb2a
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-27 21:28:43 -0500

    disabling call to MarketFeed temporarily until MarketFeed is operational

--- src/main/java/control/Engine.java
+++ src/main/java/control/Engine.java
@@ -10,11 +10,11 @@ public class Engine {
 	public static void main(String[] args){
 		MarketFeed feed = new MarketFeed("SPY");
 		
-		try {
-			double d = feed.getQuote();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			double d = feed.getQuote();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
