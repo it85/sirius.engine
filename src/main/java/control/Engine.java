1f08217afdbf74aa54c54138056bb3e70dcaf1be
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-27 23:26:08 -0500

    added dependency to library; changed Engine to use latest MarketFeed API

--- src/main/java/control/Engine.java
+++ src/main/java/control/Engine.java
@@ -1,15 +1,22 @@
 package control;
 
+import java.io.IOException;
+
+import model.CandleStick;
+import model.MarketFeed;
+
 public class Engine {
 	
 	public static void main(String[] args){
-//		MarketFeed feed = new MarketFeed("SPY");
+		MarketFeed feed = new MarketFeed("SPY", 60);
 		
-//		try {
-//			double d = feed.getQuote();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+		
+		try {
+			CandleStick d = feed.getQuote();
+			CandleStick x = d;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
