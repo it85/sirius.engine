22a669beb9ae17f5f93b65207da9c0bcafab0657
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-27 21:48:18 -0500

    disabled engine control completely temporarily

--- src/main/java/control/Engine.java
+++ src/main/java/control/Engine.java
@@ -1,14 +1,9 @@
 package control;
 
-import java.io.IOException;
-import java.math.BigDecimal;
-
-import model.MarketFeed;
-
 public class Engine {
 	
 	public static void main(String[] args){
-		MarketFeed feed = new MarketFeed("SPY");
+//		MarketFeed feed = new MarketFeed("SPY");
 		
 //		try {
 //			double d = feed.getQuote();
